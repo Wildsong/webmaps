db1c563f89198c72b9957b51dbcba0375792601b
Brian H Wilson <brian@wildsong.biz>
2019-09-19 08:43:11 -0700

    fixed routes

--- src/routesMap.js
+++ src/routesMap.js
@@ -1,14 +1,16 @@
 import { connectRoutes } from 'redux-first-router'
 import queryString from 'query-string'
 
+const base = '/maps'
+
 const routesMap = {
-    HOME:    '/',
-    MAP:     '/map',
-    ABOUT:   '/about',
-    CONTACT: '/contact',
-    FAQ:     '/faq',
-    HELP:    '/help',
-    NEWS:    '/news',
+    HOME:    base + '/',
+    MAP:     base + '/map',
+    ABOUT:   base + '/about',
+    CONTACT: base + '/contact',
+    FAQ:     base + '/faq',
+    HELP:    base + '/help',
+    NEWS:    base + '/news',
 }
 export default connectRoutes(routesMap, {
     querySerializer: queryString // This is what puts your queries into the address bar.
