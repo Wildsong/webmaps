6082c1ddf03d241bd004ae513b97d482e4275a54
Brian H Wilson <bwilson@co.clatsop.or.us>
2019-09-20 08:59:13 -0700

    added versioning information and link to project in About page

--- src/components/about.js
+++ src/components/about.js
@@ -1,14 +1,15 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import {Card, CardTitle, CardText, CardImg, CardSubtitle} from 'reactstrap'; // eslint-disable-line no-unused-vars
+import Version from '../../version'
 
 const About = () => (
   <div>
   <Card>
   <CardTitle>
-    About Map46
+    About Webmaps
   </CardTitle>
   <CardText>
-      This is a test of <a href="https://github.com/brian32768/map46#README.md">Map46</a>.
+      <Version/>
   </CardText>
   </Card>
   </div>
