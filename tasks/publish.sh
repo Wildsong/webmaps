01bbefade93a27b85778a33d2a6409ce035c2181
Brian H Wilson <bwilson@co.clatsop.or.us>
2020-10-06 14:45:39 -0700

    fixed botched merge

--- tasks/publish.sh
+++ tasks/publish.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
-# in my current set up the destination is a Windows share.
-SERVER="//cc-gis"
+# NOTE NOTE NOTE you CANNOT just share a folder under /c/inetpub/wwwroot
+# and then copy files into it, Microsoft does not like this idea even though
+# it seems elegant to me. They will break IIS when you share the folder
+# by disabling inheritance from wwwroot.
 
-echo "rebundling with parcel..."
-rm dist/*
+SERVER="cc-gis"
+
+echo "Rebundling with parcel..."
+rm -f dist/*
 parcel build index.html --public-url ./ -o default.htm --detailed-report
 
-echo "syncing to server..."
-rsync -av --delete dist/ {$SERVER}/maps
+echo "Copying files to server..."
+
+# I suppose I should clean out the folder first.
+# Or I could deploy to a different server. This 
+# one runs Windows and is therefore fragile
+# and relatively free of convenient features
+
+scp -r dist/* {$SERVER}:/c/inetpub/wwwroot/apps/webmaps-ol/
